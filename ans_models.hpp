f406a71dbfdb956cab20cbb91be846a06fec9673
Matthias Petri <Matthias.Petri@gmail.com>
2017-06-29 12:58:30 +1000

    all zero blocks are special

--- ans_models.hpp
+++ ans_models.hpp
@@ -301,7 +301,14 @@ struct ans_vbyte_model {
 
     static void model(std::vector<uint8_t>& cntsu8, uint32_t const* in, uint32_t /*sum_of_values*/, size_t n)
     {
-        if (n != ans_vbyte_model::block_size)
+        bool all_zero = true;
+        for (size_t i = 0; i < n; i++) {
+            if (in[i] != 0) {
+                all_zero = false;
+                break;
+            }
+        }
+        if (all_zero)
             return;
         auto counts = reinterpret_cast<uint64_t*>(cntsu8.data());
         for (size_t i = 0; i < n; i++) {
