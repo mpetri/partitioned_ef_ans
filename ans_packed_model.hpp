6c6fff767dc5d8a91ceafd22b4bff89ccebff160
Matthias Petri <Matthias.Petri@gmail.com>
2017-07-07 18:28:17 +1000

    max mag == 32

--- ans_packed_model.hpp
+++ ans_packed_model.hpp
@@ -3,10 +3,10 @@
 #include "block_codecs.hpp"
 
 namespace ans_packed_constants {
-const uint8_t MAX_MAG = 25;
+const uint8_t MAX_MAG = 32;
 const uint8_t NUM_MAGS = 16;
 const std::array<uint8_t, NUM_MAGS> SEL2MAG{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12,
-    14, 16, 19, 22, 25 };
+    14, 16, 19, 22, 32 };
 const std::array<uint8_t, MAX_MAG + 1> MAG2SEL{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9,
     10, 10, 11, 11, 12, 12, 13, 13, 13, 14, 14, 14, 15, 15, 15 };
 const uint64_t TOPFREQ = 1048576;
